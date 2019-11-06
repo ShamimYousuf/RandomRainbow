5f2a095bbeb5fe578f261bfd4ce373cf2ecd7c30
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2019-11-06 12:42:08 +0000

    hex to RGB converter

--- module.js
+++ module.js
@@ -1,7 +1,6 @@
 var rainbowRiches = ( function () {
 
 const addRandomColor = () => {
-
 	const RGB = generateRandomRGBvalues();
 	const hex = convertRGBtoHex(RGB);
 	document.querySelector(".color-swatch").style.backgroundColor = hex;
@@ -18,7 +17,6 @@ const convertRGBtoHex = (RGBobj) => {
 }
 
 const generateRandomRGBvalues = () => {
-
 	const randomColourCode = () =>  Math.floor((Math.random() * 255));
 	return {red: randomColourCode(), 
 		    green: randomColourCode(), 
@@ -37,6 +35,15 @@ const changeColor = (hex) => {
 	document.querySelector(".hex-value").value = hex;
 }
 
+const convertHexToRGB = (hex) => {
+	const redHex = hex.slice(1,3);
+	const greenHex = hex.slice(3,5);
+	const blueHex = hex.slice(5,7);
+	const red = parseInt(redHex, 16).toString(10);
+	const green = parseInt(greenHex, 16).toString(10);
+	const blue = parseInt(blueHex, 16).toString(10);
+	return {red, green, blue};
+}
 
 // Add color after document is loaded and ready
 window.addEventListener('load', addRandomColor);
@@ -52,11 +59,14 @@ document.querySelector('#colorpicker').addEventListener('input', function(event)
 	const hex = event.target.value;
 	document.querySelector(".color-swatch").style.backgroundColor = hex;
 	document.querySelector(".hex-value").value = hex;
+
+	const {red, green, blue} = convertHexToRGB(hex);
+	document.querySelector(".RGB-value").innerHTML = `RGB value : ${red} ${green} ${blue}`;	
 })
 
-
 return {
-	changeColor: changeColor
+	changeColor: changeColor,
+	convertHexToRGB: convertHexToRGB
 }
 
 })();
