abc1597654bd31688afd85d9adf295ee43f47a7a
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2019-11-18 17:41:50 +0000

    app

--- createRandomRainbow.js
+++ createRandomRainbow.js
@@ -10,34 +10,20 @@ var randonRainbowAppCreator =  function() {
 		header.appendChild(heading);
 	};
 
-	const displayHexValue = () => {
-
-	};
-
-	const displayRGBValue = () => {
-
-	};
-
-	const displayColorBox = () => {
-
-	};
-
-	const displayCopyHexValueButton = () => {
-
-	};
-
-	const displayNewColorButton = () => {
-
-	};
-
-	const displayChangeColorBox = () => {
-
-	};
-
 	const  createApp = () => {
 		let app = document.createElement('DIV');
 		app.setAttribute('class', 'random-rainbow-app');
 		document.getElementById('main').appendChild(app);
+
+		let colorSwatch = document.createElement('DIV');
+		colorSwatch.setAttribute('class', 'color-swatch');
+		app.appendChild(colorSwatch);
+
+		let hexLabel = document.createElement('LABEL');
+		let hexLabelText = document.createTextNode('Hex value');
+		hexLabel.appendChild(hexLabelText);
+		app.appendChild(hexLabel);
+
 	};
 
 	return {
