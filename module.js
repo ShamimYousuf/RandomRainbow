ea73b7cb23540c3ca51ecc30e2bd8bccb0ee9b9b
shamim.yousuf <shamim.yousuf@gamesys.co.uk>
2019-11-20 17:49:41 +0000

    modules

--- module.js
+++ module.js
@@ -1,9 +1,13 @@
 var rainbowRiches = function () {
 
+
+
 	const init = (selector) => {
 		createHeader(selector);
+		createApp(selector);
 	}
 
+	/* displays header as RANDOM RAINBOW */ 
 	const createHeader = (selector) => {
 		let header = document.createElement('HEADER');
 		document.querySelector(selector).appendChild(header);
@@ -12,15 +16,22 @@ var rainbowRiches = function () {
 		let headingText = document.createTextNode('RANDOM RAINBOW');
 		heading.appendChild(headingText);
 		header.appendChild(heading);
+
+		
 	};
 
-	const  createApp = () => {
+	const  createApp = (selector) => {
+
+		const RGB = generateRandomRGBvalues();
+		const hex = convertRGBtoHex(RGB);
+
 		let app = document.createElement('DIV');
 		app.setAttribute('class', 'random-rainbow-app');
-		document.getElementById('main').appendChild(app);
+		document.querySelector(selector).appendChild(app);
 
 		let colorSwatch = document.createElement('DIV');
 		colorSwatch.setAttribute('class', 'color-swatch');
+		colorSwatch.setAttribute('style', `background-color:${hex};`);
 		app.appendChild(colorSwatch);
 
 		let hexLabel = document.createElement('LABEL');
@@ -76,26 +87,26 @@ const convertHexToRGB = (hex) => {
 }
 
 
-// Add color after document is loaded and ready
-window.addEventListener('load', addRandomColor);
+// // Add color after document is loaded and ready
+// window.addEventListener('load', addRandomColor);
 
-//button generate new color
-document.querySelector('.new-color-button').addEventListener('click', addRandomColor);
+// //button generate new color
+// document.querySelector('.new-color-button').addEventListener('click', addRandomColor);
 
-//copy to clipboard listener
-document.querySelector('.copy-hex-button').addEventListener('click', copyToClipboard);
+// //copy to clipboard listener
+// document.querySelector('.copy-hex-button').addEventListener('click', copyToClipboard);
 
-//change color
-document.querySelector('#colorpicker').addEventListener('input', function(event) {
-	const hex = event.target.value;
-	document.querySelector(".color-swatch").style.backgroundColor = hex;
-	document.querySelector(".hex-value").value = hex;
+// //change color
+// document.querySelector('#colorpicker').addEventListener('input', function(event) {
+// 	const hex = event.target.value;
+// 	document.querySelector(".color-swatch").style.backgroundColor = hex;
+// 	document.querySelector(".hex-value").value = hex;
 
-	const {red, green, blue} = convertHexToRGB(hex);
-	document.querySelector(".RGB-value").innerHTML = `RGB value : ${red} ${green} ${blue}`;	
-})
+// 	const {red, green, blue} = convertHexToRGB(hex);
+// 	document.querySelector(".RGB-value").innerHTML = `RGB value : ${red} ${green} ${blue}`;	
+// })
 
-init('#main');
+
 
 return {
 	changeColor: changeColor,
@@ -105,5 +116,4 @@ return {
 
 };
 
-rainbowRiches();
 
